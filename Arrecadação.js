6ad4c2cc278779d6c8d83595354560ae0991d12c
Wilian Wagner <wilianwagnerwork@gmail.com>
2022-10-03 21:38:05 -0300

    Versão final do js, com lógica finalizada e funcionando

--- Arrecadação.js
+++ Arrecadação.js
@@ -1,46 +1,55 @@
+
+//Definição do valor do ingresso
 const ingresso = 10
 const ingressoVip = 15
 const ingressoEstudante = 5
+
+//definição e contagem dos tipos de ingresso
+var contNor = 0
+var contVip = 0
+var contEst = 0
+
+//variaveis de entrada
 var nome = []
 var tipo = []
-//var ingressosTotal  = Number(prompt("Digite a quantidade de ingressos vendidos: "))
 
-var somatorio = 0
-
+//variavel utilizada para fim de entrada de informações
 let fim = 0
+//variavel para arrecadação total
+let total = 0
 
-while (fim  != true ) {
-    nome = prompt("Digite o nome do comprador:")
-    tipo = prompt("Digite o numero do tipo de ingresso, sendo os tipos 1-Normal, 2- VIP, 3-Estudante. Para finalizer digite 'pronto' ")
- fim = tipo.includes("pronto")
-    console.log(tipo)
-}
-console.log("deu")
-
-/*<!doctype html><html><head></head><body><script type="text/javascript">//cálculo de média para "N" notas escolares
-
-//"N" notas
-let notas = []
-let nNotas = Number(prompt("DIGITE A QUANTIDADE DE NOTAS: "))
-let somatorio = 0
-let media = 0
-
-for (let iNotas = 0; iNotas < nNotas; iNotas++) {
-  notas [iNotas] = Number(prompt(`DIGITE A NOTA Nº ${iNotas+1}: `))
-  somatorio = somatorio + notas[iNotas] //soma das notas
+//inserção de dados  através de um loop
+for (let i = 0; fim != true; i++) {
+  nome[i] = prompt("Digite o nome do comprador:")
+  console.log(nome)
+  tipo[i] = prompt("Digite o numero do tipo de ingresso, sendo os tipos 1-Normal, 2- VIP, 3-Estudante. Para finalizer digite 'pronto' ")
+  fim = tipo.includes("pronto")
+  console.log(tipo)
 }
 
-/*
-//teste para conferir se todas as notas digitadas estão armazenadas corretamente:
-for (let iNotas = 0; iNotas < nNotas; iNotas++) {
-  console.log(notas[iNotas])
+//contagem do tipo de ingresso
+for (let i = 0; i < tipo.length; i++) {
+  if (tipo[i] === "1") { contNor++; }
 }
+console.log(contNor)
 
+//contagem do tipo de ingresso
+for (let i = 0; i < tipo.length; i++) {
+  if (tipo[i] === "1") { contEst++; }
+}
+console.log(contEst)
 
-media = somatorio / nNotas
-console.log(`A SUA MÉDIA É ${media}!`)
+//contagem do tipo de ingresso
+for (let i = 0; i < tipo.length; i++) {
+  if (tipo[i] === "1") { contVip++; }
+}
+console.log(contVip)
 
-//iNotas = indíce de notas, vai indicar a possição
-//nNotas = número total de notas
+//Calculo de arrecadação total
+total = (contEst * ingressoEstudante) + (contNor * ingresso) + (contVip * ingressoVip)
 
-</script></body><html></html>*/
+//impressão de cada arrecadação
+console.log(`Arrecadação total foi de R$ ${total}`)
+console.log(`Arrecadação de Ingressos normais foi de : $ ${contNor * ingresso}`)
+console.log(`Arrecadação de Ingressos Vip foi de : $ ${contVip * ingressoVip}`)
+console.log(`Arrecadação de Ingressos Estudante foi de : $ ${contEst * ingressoEstudante}`)
